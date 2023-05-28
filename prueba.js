7f9c5d0e33184e5cfaf4f31c809ffb6baef247e6
DaissyM <daissymg1998@gmail.com>
2023-05-27 19:34:26 -0500

    checkpoint commit

--- prueba.js
+++ prueba.js
@@ -1,6 +1,14 @@
 
-   
 
+function agregarAmigo(objetoUsuario, nuevoAmigo) {
+    // El parámetro "objetoUsuario" tiene una propiedad llamada "amigos" igual a un arreglo.
+    // Debes agregar el "nuevoAmigo" al final de este arreglo.
+    // Retornar el objeto.
+    // Tu código:
 
+    objetoUsuario.amigos.push(nuevoAmigo)
+    return(objetoUsuario)
 
-console.log(palabraMasLarga(["hola","mundo"]))
+}
+ console.log(agregarAmigo('casa',12))
+ 
